e0f0d631cace9a97fd1562ada0ac62b2ce88f2bf
charles32110 <596147986@qq.com>
2019-04-08 17:37:33 +0800

    data_structure

---
+++ data_structure/src/ListNode.java
@@ -0,0 +1,13 @@
+/**
+ * charles
+ * java learn
+ * 2019-04-08
+ */
+public class ListNode {
+    public ListNode(int value) {
+        this.value = value;
+    }
+
+    public int value;
+    public ListNode next;
+}
